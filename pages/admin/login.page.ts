a792f4e20c4cddad93f6aa06cdcf1317390e6277
Izazur Rahman Shafin <shafin97@outlook.com>
2025-05-26 15:08:03 +0600

    Assignment 1

---
+++ pages/admin/login.page.ts
@@ -0,0 +1,29 @@
+import { Page, Locator } from "@playwright/test";
+import { BasePage } from "./base.page";
+import testdata  from "../../fixtures/testdata.json";
+
+export class LoginPage extends BasePage {
+    readonly email: Locator;
+    readonly password: Locator;
+    readonly loginButton: Locator;
+    
+
+    constructor(page: Page) {
+        super(page);
+        this.email = page.getByRole('textbox', { name: 'Email' });
+        this.password = page.getByRole('textbox', { name: 'Password' });
+        this.loginButton = page.getByRole('button', { name: 'Log In'});
+    }
+
+    async navigate() {
+        await this.page.goto(testdata["validUser"]["baseurl"]);
+    }
+
+    async login(email: string, password: string) {
+        await this.email.fill(email);
+        await this.password.fill(password);
+        await this.loginButton.click();
+    }
+}
+
+//workflow management

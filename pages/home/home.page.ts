98d5d1b59ce10eda4d45c4ef3cd9c6ddff435158
Izazur Rahman Shafin <shafin97@outlook.com>
2025-06-01 15:40:11 +0600

    Dome

--- pages/home/home.page.ts
+++ pages/home/home.page.ts
@@ -3,14 +3,17 @@ import { BasePage } from "../base.page";
 
 export class HomePage extends BasePage {
     readonly dashboard: Locator;
+    applications: Locator;
 
     constructor(page: Page) {
         super(page);
-      //  this.dashboard = page.getByRole('button', { name: 'Applications' });
+        this.applications = page.getByRole('button', { name: 'Applications' });
     }
 
     async navigate(url: string) {
         await this.page.goto(url);
+        await this.applications.click();
+        //await this.ICSOP.click();('button', { name: 'Investment Clearance' }).getByRole('link').click();
     }
 
     async operations() {
